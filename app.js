3f867137f2625b5cb647486237a29775bb312564
ousmane <ouzdeville@gmail.com>
2020-03-30 00:15:19 +0000

    dfd

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var mongoose = require("mongoose");
 require('dotenv').config();
 var CronJob = require('cron').CronJob;
+var cors = require('cors');
 
 
 var usersRouter = require('./routes/users');
@@ -17,14 +18,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(process.env.DB_PASS);
+console.log(process.env.DB_USER);
 mongoose.connect(
   "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS+"@cluster0-wfwzr.mongodb.net/test?retryWrites=true&w=majority",
   { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }
