8b96afe6c171cf1778360618a4488f22d82c453f
ousmane <ouzdeville@gmail.com>
2020-04-08 23:33:39 +0000

    make it better

--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router.route("/addposition").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route("/:telnumber/:debut/:fin").get((req, res) => {
+router.route("/:telnumber/:debut/:fin/:checked").get((req, res) => {
   console.log('Adding time: ' + req.params.telnumber);
   User.find({ 
       telnumber: req.params.telnumber,
